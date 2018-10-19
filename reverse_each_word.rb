630ffd9e3c271c5629c73768439a9b1d870e515d
Paul Clark <clark.paul123@gmail.com>
2018-10-19 15:51:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,11 @@ def reverse_each_word(sentence)
   end
   reversed.join(" ")
 end
+
+def reverse_each_word(sentence)
+    arr_split = sentence.split(" ")
+    reversed = []
+    arr_split.collect do |word| reversed << word.reverse
+  end
+  reversed.join(" ")
+end
